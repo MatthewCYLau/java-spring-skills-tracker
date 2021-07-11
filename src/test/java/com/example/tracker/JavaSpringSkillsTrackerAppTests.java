b34c831451f49a19525590af90affc2fbdf68076
Matthew Lau <matthew.lau@matthews-mbp.home>
2021-07-11 22:17:16 +0100

    Add dummy

--- src/test/java/com/example/tracker/JavaSpringSkillsTrackerAppTests.java
+++ src/test/java/com/example/tracker/JavaSpringSkillsTrackerAppTests.java
@@ -1,13 +1,28 @@
 package com.example.tracker;
 
+import com.example.tracker.profile.ProfileController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class JavaSpringSkillsTrackerAppTests {
 
-	@Test
-	void contextLoads() {
-	}
+
+    @Autowired
+    private ProfileController controller;
+
+    @Test
+    public void contextLoads() throws Exception {
+        assertThat(controller).isNotNull();
+    }
+
+    @Test
+    public void exampleTest() {
+        assertEquals("foo", "foo");
+    }
 
 }
