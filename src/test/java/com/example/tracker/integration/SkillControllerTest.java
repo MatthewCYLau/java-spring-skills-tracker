61e99ba171419b0b24cae0472773a2481047e3e0
Matthew Lau <matthew.lau@matthews-mbp.home>
2021-07-22 14:13:21 +0100

    Tried auto-wiring to talk to service

--- src/test/java/com/example/tracker/integration/SkillControllerTest.java
+++ src/test/java/com/example/tracker/integration/SkillControllerTest.java
@@ -4,23 +4,32 @@ import com.example.tracker.skill.SkillController;
 import com.example.tracker.skill.SkillService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@ContextConfiguration(classes = PasswordEncoder.class)
-@WebMvcTest(SkillController.class)
+
+//@ContextConfiguration(classes = PasswordEncoder.class)
+//@WebMvcTest(SkillController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class SkillControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+//    @MockBean
+    @Autowired
     private SkillService skillService;
 
 //    returns 401 for unsecurted ULRs
@@ -28,5 +37,10 @@ class SkillControllerTest {
     @Test
     public void shouldReturnSkills() throws Exception {
         this.mockMvc.perform(get("/api/v1/skills")).andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    public void shouldReturnSkillsFromService() throws Exception {
+        assertEquals(skillService.getSkills(), "foo");
     }
 }
